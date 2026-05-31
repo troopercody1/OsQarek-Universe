f1d0c1c590b6fa5a1a732352729b2848d212769c
troopercody1 <codyiliffe985@gmail.com>
2026-05-30 21:36:35 -0400

    Delete badwords.js containing inappropriate content
    Removed offensive words and phrases from the badwords list.

--- badwords.js
+++ badwords.js
@@ -1,11 +1,1 @@
-module.exports = [
-    "kys",
-    "kill yourself",
-    "nigger",
-    "nigga",
-    "free nitro",
-    "nitro gift",
-    "steam-gift",
-    "discord.gg/",
-    "gift.nitro"
-];
+
