56c246e926b27f7efbebdbabac3b1c775f0b999f
troopercody1 <codyiliffe985@gmail.com>
2026-06-03 16:09:53 -0400

    Remove unused path module import

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ process.on('unhandledRejection', (reason, promise) => console.error('Unhandled P
 
 // -- ADVANCED INTERACTIVE DASHBOARD WITH DISCORD OAUTH2 (RENDER PATCHED) --
 const session = require('express-session');
-const path = require('path');
 
 // CRASH PROTECTION
 process.on('uncaughtException', (err) => console.error('CRITICAL DASHBOARD ERROR:', err));
