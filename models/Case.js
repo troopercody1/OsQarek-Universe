ce6e1047a7155c1979b598ba1c4e2b16cedaf819
troopercody1 <codyiliffe985@gmail.com>
2026-06-03 22:14:26 -0400

    Add Case model schema using Mongoose

--- models/Case.js
+++ models/Case.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const caseSchema = new mongoose.Schema({
+    id: Number,
+    type: String,
+    user: String,
+    userId: String,
+    reason: String,
+    moderator: String,
+    timestamp: Date
+});
+
+module.exports = mongoose.model('Case', caseSchema);
