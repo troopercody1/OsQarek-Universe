9c59b34ea3ce95c560cd6e2b14179c409f24745b
troopercody1 <codyiliffe985@gmail.com>
2026-06-03 22:14:02 -0400

    Add User model with schema definition

--- models/User.js
+++ models/User.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    userId: { type: String, required: true, unique: true },
+    offences: { type: Number, default: 0 },
+    staffStrikes: { type: Number, default: 0 },
+    notes: [{
+        note: String, 
+        mod: String,
+        createdAt: { type: Date, default: Date.now }
+    }]
+});
+
+module.exports = mongoose.model('User', userSchema);
